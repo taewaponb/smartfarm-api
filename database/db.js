071b92d769aa61d97e96e1cc8873b00756254a73
EarthSurawit <surawitwiriya@kkumail.com>
2020-01-03 16:32:48 +0700

    version 0.0.13

--- database/db.js
+++ database/db.js
@@ -1,5 +1,5 @@
 var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb://139.180.129.74:27017/";
+var url = "mongodb://45.77.39.58:27017/";
 const con = MongoClient;
 con.connect(url, function(err, db) {
   if (err) {
