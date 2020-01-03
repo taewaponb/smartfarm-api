151bb16bef5fba8ea6183b5ad6277f46f53426eb
EarthSurawit <surawitwiriya@kkumail.com>
2020-01-03 16:36:19 +0700

    version 0.0.14

--- database/db.js
+++ database/db.js
@@ -8,19 +8,19 @@ con.connect(url, function(err, db) {
     console.log("MongoDB is Connected");
     db.db;
     var dbo = db.db("smartfarmDB");
-    // var userInfo = [];
-    // dbo.collection("users").findOne({}, function(err, result) {
-    //   if (!err) {
-    //     console.log("id = " + result._id);
-    //     console.log("name = " + result.name);
-    //     userInfo.push({
-    //       userId: result._id,
-    //       username: result.name
-    //     });
-    //     console.log(userInfo);
-    //     db.close();
-    //   }
-    // });
+    var userInfo = [];
+    dbo.collection("users").findOne({}, function(err, result) {
+      if (!err) {
+        console.log("id = " + result._id);
+        console.log("name = " + result.name);
+        userInfo.push({
+          userId: result._id,
+          username: result.name
+        });
+        console.log(userInfo);
+        db.close();
+      }
+    });
   }
 });
 
